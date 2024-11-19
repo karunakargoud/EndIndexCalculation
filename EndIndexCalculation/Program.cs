e60ec7aad789f05616be50d998e8bec836b991c7
karunakargoud <karunakargoud1116@gmail.com>
2024-11-19 12:13:18 +0530

    FirstCommit

---
+++ EndIndexCalculation/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EndIndexCalculation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "software application is used to access information";
+            string substring = "tion";
+            int count = 0;
+            for (int i = 0; i <= input.Length - substring.Length; i++)
+            {
+                if (input.Substring(i,substring.Length)== substring)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+            Console.ReadLine();
+        }
+    }
+}
